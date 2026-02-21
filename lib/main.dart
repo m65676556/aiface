ca7f7321789e253e58f0d7fd7141acfb21d4ce24
a1520588207 <a1520588207@yahoo.com>
2026-02-21 23:48:32 +0100

    fix: register shared_preferences_web manually for web build

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_platform_interface.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    SharedPreferencesStorePlatform.instance = SharedPreferencesPlugin();
+  }
   runApp(
     const ProviderScope(
       child: AIFaceApp(),
