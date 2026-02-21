93f9f6035687cc896c3e02111845b6404da375ce
a1520588207 <a1520588207@yahoo.com>
2026-02-21 22:23:16 +0100

    fix: use ESM export default for Next.js API routes

--- backend/pages/api/messages.js
+++ backend/pages/api/messages.js
@@ -1,7 +1,7 @@
-const { sql } = require('@vercel/postgres');
-const { randomUUID } = require('crypto');
+import { sql } from '@vercel/postgres';
+import { randomUUID } from 'crypto';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
--- backend/pages/api/profile.js
+++ backend/pages/api/profile.js
@@ -1,6 +1,6 @@
-const { sql } = require('@vercel/postgres');
+import { sql } from '@vercel/postgres';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
